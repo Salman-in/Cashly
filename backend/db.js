cb230b4aae4364fd95a0d0d6685a1671f35be580
Salman <salmannajah123@gmail.com>
2025-02-09 13:34:29 +0530

    Added Accounts DB

--- backend/db.js
+++ backend/db.js
@@ -4,14 +4,50 @@ const MONGO_URL = process.env.MONGO_URL;
 mongoose.connect(MONGO_URL);
 
 const userSchema = new mongoose.Schema({
-    firstname: String,
-    lastname: String,
-    username: String,
-    password: String,
+    firstname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+})
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    balance: {
+        type: Number,
+        default: 0,
+        required: true
+    },
 })
 
 const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", accountSchema);
 
 module.exports = {
-    User
+    User, Account
 }; 
